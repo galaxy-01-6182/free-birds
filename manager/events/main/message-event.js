9b06404530e704c2872b666ccc50f6df4762f7bb
Galax <73770251+galaxy-01-6182@users.noreply.github.com>
2021-02-28 12:43:47 +0530

    Update message-event.js
    Removed useless whitespaces and changed user ids

--- manager/events/main/message-event.js
+++ manager/events/main/message-event.js
@@ -5,33 +5,28 @@ module.exports = {
 	name: 'message',
 	execute(client, message){
 		
-		message.content = message.content.toLowerCase();
-		
+	 message.content = message.content.toLowerCase();		
 	 
 	 if(message.author.bot) return;
 	
-	if(client.config.developMode){
+	if(client.config.developMode){		
 		
-		
-		 if(message.guild && message.guild.id == "747696876379439205") return;
-	 
-	 if(message.author.id !== "729907149479870585") return;
+	    if(message.guild && message.guild.id == client.data.home.id) return;
+	
+ 
+	 if(message.author.id !== client.data.owner.user.id) return;
 	 	 
-	 	message.channel.send("Processing Work...");
-	 	 
+	 	 message.channel.send("Processing Work...");
+ 	 
 	 	 processor.run(client, message);
 	 	
 	
 	 } else {
 	 	
-	 	 processor.run(client, message);	  
+	     processor.run(client, message);	  
 	 	 	 	
 	 }
-	 
-	 
-		
-		
-	},
-		
+	 	 				
+    },		
 	
 }
