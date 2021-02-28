8b09ad0adca78527d14dd28d9d31213021c1aa41
Galax <73770251+galaxy-01-6182@users.noreply.github.com>
2021-02-28 12:33:48 +0530

    Edit ready-event.js
    Removed useless whitespaces

--- manager/events/prepare/ready-event.js
+++ manager/events/prepare/ready-event.js
@@ -1,5 +1,5 @@
 const manager = require('../../main.js')
- const processor = require('../../../processor/main.js')
+const processor = require('../../../processor/main.js')
 
 module.exports = {
 	
@@ -9,15 +9,13 @@ module.exports = {
 		
 	  	await manager.derive(client)
  
-    await    processor.attachHandler(client);
-   
-   await processor.attachCooldowner(client)
+                await processor.attachHandler(client);
+    
+                await processor.attachCooldowner(client)
   
-   await manager.setup(client);
+                await manager.setup(client);
 	 
-	  console.log("Bird is up!");	  
+	  console.log("Bird is up!");	  		
 		
-		
-	},
-	
+	},	
 }
