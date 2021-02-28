77006ecfb227196670fc389f24a291354a500d66
Galax <73770251+galaxy-01-6182@users.noreply.github.com>
2021-02-28 12:44:43 +0530

    Added activate.js
    Imported activate.js from local to git

---
+++ manager/activate.js
@@ -0,0 +1,33 @@
+ const fs = require('fs')
+
+ const main = require('./main.js')
+ const processor = require('../processor/main.js')
+
+module.exports.run = (client) => {
+	
+const eventPath = './manager/events'
+
+const eventFolders =    fs.readdirSync(eventPath)
+
+for (const folder of eventFolders) { 
+
+
+   let eventFiles = fs.readdirSync(`${eventPath}/${folder}`).filter(file => file.endsWith('-event.js'))
+
+ for(const eventFile of eventFiles){
+
+	const event = require(`./events/${folder}/${eventFile}`);
+	
+	  	if (event.once) { 	
+	  	
+	  		client.once(event.name, (...args) => event.execute(client, ...args)); 
+	  		
+	  			} else { 	
+	  			
+	  				client.on(event.name, (...args) => event.execute(client, ...args)); 	
+	  					  				
+	     	}
+	   }
+	 }
+ 
+ }
